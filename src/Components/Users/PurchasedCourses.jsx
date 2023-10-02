aa4fd35d2586bd41c4f6d9b736fad1bb70e67b7e
shreyanshi526 <shreyanshikushwaha526@gmail.com>
2023-10-02 10:52:44 +0530

    third commit

--- src/Components/Users/PurchasedCourses.jsx
+++ src/Components/Users/PurchasedCourses.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import React, { useState } from 'react'
 
 const PurchasedCourses = () => {
+  const [PurchasedCourse, setPurchasedCourse] = useState([]);
+
+  const handleClick = () => {
+    fetch('http://localhost:3000/users/purchasedCourses', {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("token")
+      }
+    }).then((Response) => {
+      return Response.json();
+    }).then((data) => {
+      console.log(data);
+      setPurchasedCourse(data);
+    })
+  }
+
+  // React.useEffect(() => {
+  //   fetch('http://localhost:3000/users/purchasedCourses', {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-type": "application/json",
+  //       "Authorization": "Bearer " + localStorage.getItem("token")
+  //     }
+  //   }).then((res) => {
+  //     res.json().then((data) => {
+  //       setPurchasedCourse(data)
+  //     })
+  //   })
+  // }, [])
+
+
+
   return (
     <>
-       <div>
+      <div>
         welcome !!
-       </div>
+        <button onClick={handleClick}>view</button>
+
+      </div >
     </>
   )
 }
--- src/Components/Users/UserProfile.jsx
+++ src/Components/Users/UserProfile.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import PurchasedCourses from './PurchasedCourses.jsx'
+import ListAllCourses from './ListAllCourses.jsx'
 
 const UserProfile = () => {
   return (
     <>
-      weolcome to your profile!!
+      welcome to your profile!!
+
+      <div style={{width:"60vmax",height:"40vmax", border:"0.4vmax solid black"}}>
+        <h1>your courses</h1>
+       <PurchasedCourses></PurchasedCourses>
+      </div>
+      <div style={{width:"60vmax",height:"40vmax", border:"0.4vmax solid black"}}>
+        <h1>ALL courses</h1>
+       <ListAllCourses></ListAllCourses>
+      </div> 
     </>
   )
 }
 
-export default UserProfile
+export default UserProfile;
